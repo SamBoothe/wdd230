d29b4ea1db94a534a2e2d84cd57360e597847810
SamBoothe <boo22002@byui.edu>
2022-09-22 21:42:35 -0600

    Updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,3 @@ let date = document.querySelector("#currentdate");
 date.textContent = new Date();
 
 let currentDate = new Date();
-let time = currentDate.getHours()
-document.querySelector("#currenttime");
-
-time.textContent = new Date().toLocaleTimeString();
